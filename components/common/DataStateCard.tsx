cd9383960433c43973258bc5e3a360b59d790cd5
OSAMA AMER <71990545+osama-amer60@users.noreply.github.com>
2025-11-07 18:49:24 +0400

    refactor: Move noDataFoundCardStyle to DataStateCard component and remove unused utility file

--- components/common/DataStateCard.tsx
+++ components/common/DataStateCard.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { noDataFoundCardStyle } from "@/utils/common"
-import { Card, Spin, Typography } from "antd"
+import { Card, Grid, Spin, Typography } from "antd"
 
 const { Text } = Typography
+const { useBreakpoint } = Grid
 
 interface DataStateCardProps {
   isLoading?: boolean
@@ -16,6 +16,19 @@ export const DataStateCard = ({
   isError,
   errorMessage = "Failed to load data. Please try again.",
 }: DataStateCardProps) => {
+  const screens = useBreakpoint()
+  const isMobile = !screens.md
+
+  const noDataFoundCardStyle: React.CSSProperties = {
+    background: "transparent",
+    color: "#FFF",
+    border: "none",
+    minHeight: isMobile ? 650 : 350,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }
+
   if (isLoading) {
     return (
       <Card style={noDataFoundCardStyle}>
--- utils/common.ts
+++
@@ -1,9 +0,0 @@
-export const noDataFoundCardStyle = {
-  background: "transparent",
-  color: "#FFF",
-  border: "none",
-  minHeight: 350,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-}
